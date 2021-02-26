481280c9f0b4bedb0a99442e349b59dfb30a8ebd
Mingxun Wang <mwang87@gmail.com>
2021-02-26 13:11:12 -0800

    adding similarity

--- run_simile.py
+++ run_simile.py
@@ -2,16 +2,6 @@ import simile.simile as sml
 import numpy as np
 
 def calculate_simile(spectrum1_dict, spectrum2_dict):
-    #mz1 = [peak[0] for peak in spectrum1_dict["peaks"]]
-    #int1 = [peak[1] for peak in spectrum1_dict["peaks"]]
-
-    #s1 = np.asarray([mz1, int1]).astype(float)
-
-    #mz2 = [peak[0] for peak in spectrum2_dict["peaks"]]
-    #int2 = [peak[1] for peak in spectrum2_dict["peaks"]]
-
-    #s2 = np.asarray([mz2, int2]).astype(float)
-
     s1 = spectrum1_dict
     s2 = spectrum2_dict
 
@@ -36,11 +26,8 @@ def calculate_simile(spectrum1_dict, spectrum2_dict):
     # Calculate significance of the alignment
     pval = sml.alignment_test(S, mzi1[0], mzi2[0])
 
-
     scores = {}
     scores["score"] = simile_score
     scores["pval"] = pval
-
-    #print(scores)
 
     return scores
