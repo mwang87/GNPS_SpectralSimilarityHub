e3f7cae2c97c55e612d8975a365aee1071bae008
Mingxun Wang <mwang87@gmail.com>
2021-04-26 15:57:19 -0700

    updating url

--- test-integration/test-integration.py
+++ test-integration/test-integration.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def test_usi_api():
-    url = "{}/api/comparison".format("http://dorresteintesthub.ucsd.edu:5453")
+    url = "{}/api/comparison".format("https://gnps-similarity.ucsd.edu")
     params = {}
     params["usi1"] = "mzspec:GNPS:TASK-c95481f0c53d42e78a61bf899e9f9adb-spectra/specs_ms.mgf:scan:1943"
     params["usi2"] = "mzspec:GNPS:TASK-c95481f0c53d42e78a61bf899e9f9adb-spectra/specs_ms.mgf:scan:1969"
@@ -12,7 +12,7 @@ def test_usi_api():
     print(r.json())
 
 def test_peaks_api():
-    url = "{}/api/comparison".format("http://dorresteintesthub.ucsd.edu:5453")
+    url = "{}/api/comparison".format("https://gnps-similarity.ucsd.edu")
 
     spec1 = requests.get("https://metabolomics-usi.ucsd.edu/json/?usi={}".format("mzspec:GNPS:TASK-c95481f0c53d42e78a61bf899e9f9adb-spectra/specs_ms.mgf:scan:1943"))
     spec2 = requests.get("https://metabolomics-usi.ucsd.edu/json/?usi={}".format("mzspec:GNPS:TASK-c95481f0c53d42e78a61bf899e9f9adb-spectra/specs_ms.mgf:scan:1969"))
